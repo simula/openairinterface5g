78a78fdb362bd8e66ae1bba5cfc0eec6bb3f072e
claymilos <clay@milos.co.za>
2026-07-26 15:10:34 +0000

    Added config for the NKOM frequencies in 20MHz sections 2300-2320, 2320-2340, 2340-2360 and 2360-2380.

---
+++ run-gNodeB-n40-2300-2320.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash -eu
+
+# Assuming 210!
+
+ping -c3 -i0.2 10.133.211.35   # AMF
+
+#cd /root/openair5G
+source oaienv
+cd cmake_targets/ran_build/build
+#nice -n -19 ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/dsb.conf --gNBs.[0].min_rxtxtime 6 --sa --usrp-tx-thread-config 1 -E
+nice -n -19 ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/nkom-24269-n40-2300-2320.conf --gNBs.[0].min_rxtxtime 6 --usrp-tx-thread-config 1 -E
+#nice -n -19 ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/dsb.conf.2300MHz --gNBs.[0].min_rxtxtime 6 --sa --usrp-tx-thread-config 1 -E
+
+
